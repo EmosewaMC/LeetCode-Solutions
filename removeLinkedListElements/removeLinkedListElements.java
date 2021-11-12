426baa8a874c80ebb00bc5cda14f058ac79cfc28
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2021-11-12 03:09:08 -0800

    Added removeLinkedListElements

---
+++ removeLinkedListElements/removeLinkedListElements.java
@@ -0,0 +1,42 @@
+package removeLinkedListElements;
+
+public class removeLinkedListElements {
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode temp = head;
+        ListNode prev = null;
+        ListNode next = null;
+        while(temp != null) {
+            // temp = temp.next;
+            // if(temp != null && temp.val == val) {
+            //     prev.next = temp.next;
+            //     temp.next = null;
+            //     temp = prev;
+            // } else if(temp == null && prev.val == val) {
+            //     head = null;
+            //     temp = null;
+            // }
+            // prev = temp;
+            if(temp != null && temp.val == val) {
+                next = temp.next;
+                temp = null;
+                temp = next;
+            } else {
+                temp = temp.next;
+
+            }
+        }
+        return head;
+    }
+}
+}
