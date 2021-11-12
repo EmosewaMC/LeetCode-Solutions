45a06d4b73a2dc99d0e2af54c87acd3b51c22119
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2021-11-12 01:48:13 -0800

    Added Already Solved LeetCode problems
    Added AddTwoNumbers, Palindrome, and TwoSum.  Created and verified that ListNode works.

---
+++ twoSum/TwoSum.java
@@ -0,0 +1,22 @@
+package twoSum;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        
+        for(int i = 0; i < list.size(); i++) {
+            int temp = list.get(i);
+            list.remove(i);
+            if(list.contains(target - temp)) {
+                int location = list.indexOf(target - temp) + 1;
+                return new int[]{i, location};
+            }
+            list.add(i, temp);
+        }
+        return nums;
+    }
+}
